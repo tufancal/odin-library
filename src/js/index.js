ccead4d29082569fa3f1f906b9c3d054c19d1d0d
Tufan Calisir <tufan.calisir@project-joker.com>
2022-10-30 01:34:25 +0200

    refactor: cleaned code structure

--- src/js/index.js
+++ src/js/index.js
@@ -4,22 +4,21 @@ import '../scss/index.scss';
 const addBook = document.querySelector('.add-js');
 const submitBook = document.querySelector('.submit-js');
 const modal = document.querySelector('.modal-js');
-const container = document.querySelector('.container');
-const error = document.querySelector('.error');
-const formElements = document.querySelectorAll('.bookform__element');
+const container = document.querySelector('.container-js');
+const error = document.querySelector('.error-js');
+const form = document.querySelector('.form-js');
 
 // pre-defined libray for array of objects
 let library = [];
 // Data-set attribute to check remove
 const datasetAttribute = 'book';
 
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  console.log(this.read);
 }
 
 function createBookTile(element) {
@@ -36,14 +35,13 @@ function createBookTile(element) {
   title.classList.add('tile__title', 'tile__element');
   author.classList.add('tile__element');
   pages.classList.add('tile__element');
-  read.classList.add('tile__read', 'tile__button');
-  remove.classList.add('tile__read', 'tile__button');
+  read.classList.add('tile__read', 'tile__read-js', 'tile__button');
+  remove.classList.add('tile__remove', 'tile__remove-js', 'tile__button');
 
   //Values
   title.innerHTML = '"' + element.title + '"';
   author.innerHTML = element.author;
   pages.innerHTML = element.pages;
-  console.log(element.read);
 
   if (element.read) {
     read.innerHTML = 'Read';
@@ -66,34 +64,8 @@ function createBookTile(element) {
   return tile;
 }
 
-container.addEventListener('click', element => {
-  console.log(element.target.parentNode);
-  console.log(
-    element.target.previousElementSibling.classList.contains('title')
-  );
-
-  if (element.target.previousElementSibling.classList.contains('title')) {
-    console.log(element.target.previousElementSibling.innerHTML);
-  }
-
-  for (let i = 0; i < library.length; i++) {
-    const foo = library[i].title;
-
-    if (element.target.previousElementSibling.classList.contains('title')) {
-      if (foo == element.target.previousElementSibling.innerHTML) {
-        console.log('Check');
-        library = library.splice(i, 1);
-        console.log(library);
-      }
-    }
-  }
-  if (element.target.dataset.index == element.target.parentNode.dataset.index) {
-    element.target.parentNode.remove();
-  }
-});
-
 function resetForm() {
-  document.getElementById('bookform').reset();
+  form.reset();
 }
 
 function addBookToLibrary(object) {
@@ -125,11 +97,26 @@ submitBook.addEventListener('click', () => {
     resetForm();
     modal.close();
   }
-
-  console.log(library);
 });
 
-//Todo:
-// blank html when creating tile corretly
-// check library (for loop) with created div e.g. with title and delete it also from array as you delete it from dom
-// styling
+container.addEventListener('click', element => {
+  const target = element.target;
+  const firstChild = target.parentNode.firstChild;
+  const firstChildInner = firstChild.innerHTML.replace(/"/g, '');
+  const remove = target.classList.contains('tile__remove-js');
+
+  if (remove && target.dataset.index === target.parentNode.dataset.index) {
+    for (let i = 0; i < library.length; i++) {
+      const title = library[i].title;
+
+      if (
+        firstChild.classList.contains('tile__title') &&
+        title == firstChildInner
+      ) {
+        library.splice(i, 1);
+        target.parentNode.remove();
+        break;
+      }
+    }
+  }
+});
